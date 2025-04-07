98d21387fc0908eedbe39ee24aea347511899aa9
Artem Filippow <a.filippov@qtech.ru>
2025-04-07 17:25:05 +0900

    add private props

--- src/category.py
+++ src/category.py
@@ -10,7 +10,15 @@ class Category:
     def __init__(self, name: str, descriptions: str, products: list[Product]):
         self.name = name
         self.description = descriptions
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         self.product_count = len(products)
         Category.product_count += self.product_count
+
+    def add_product(self, product: Product) -> None:
+        self.__products.append(product)
+        self.product_count += 1
+
+    @property
+    def products(self) -> list:
+        return list(map(lambda x: f"{x.name}, {x.price} руб. Остаток {x.quantity} шт.", self.__products))
--- src/product.py
+++ src/product.py
@@ -1,8 +1,31 @@
+from typing import Self
+
+
 class Product:
     """класс Товар"""
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_dict: dict) -> Self:
+        return cls(product_dict["name"], product_dict["description"], product_dict["price"], product_dict["quantity"])
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, p: float) -> None:
+        if p <= 0:
+            print("Цена не должна быть нулевая или отрицательна")
+        else:
+            if p < self.__price:
+                confirm = input("Цена ниже текущей. Подтверждаете? y/n")
+                if confirm.lower() == "y":
+                    self.__price = p
+            else:
+                self.__price = p
