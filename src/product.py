d62175cbc20d8b1987f9a04cb416d42b9c0063e1
Artem Filippow <a.filippov@qtech.ru>
2025-04-22 13:50:15 +0900

    print product on creation

--- src/product.py
+++ src/product.py
@@ -1,8 +1,21 @@
 from typing import Any, Self
 
 
-class Product:
-    """класс Товар"""
+class PrintProduct:
+    ID = 1
+
+    def __init__(self) -> None:
+        self.id = self.ID
+        PrintProduct.ID += 1
+        print(f"[Product created ID: {self.id}]", self)
+
+
+class BaseProduct:
+    def __init__(self, name: str, description: str, price: float, quantity: int):
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
 
     def __str__(self) -> str:
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
@@ -12,16 +25,6 @@ class Product:
             return self.quantity * self.price + other.quantity * other.price
         else:
             raise TypeError
-
-    def __init__(self, name: str, description: str, price: float, quantity: int):
-        self.name = name
-        self.description = description
-        self.__price = price
-        self.quantity = quantity
-
-    @classmethod
-    def new_product(cls, product_dict: dict) -> Self:
-        return cls(product_dict["name"], product_dict["description"], product_dict["price"], product_dict["quantity"])
 
     @property
     def price(self) -> float:
@@ -38,6 +41,18 @@ class Product:
                     self.__price = p
             else:
                 self.__price = p
+
+
+class Product(BaseProduct, PrintProduct):
+    """класс Товар"""
+
+    def __init__(self, name: str, description: str, price: float, quantity: int):
+        super().__init__(name, description, price, quantity)
+        PrintProduct.__init__(self)
+
+    @classmethod
+    def new_product(cls, product_dict: dict) -> Self:
+        return cls(product_dict["name"], product_dict["description"], product_dict["price"], product_dict["quantity"])
 
 
 class Smartphone(Product):
